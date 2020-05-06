656db49c38da476d3857e148870004ea4efbd098
cpadilla <cpadilla@PC01TEC77.cobiscorp.int>
2020-05-06 16:52:06 -0500

    Completed feature 3 search by category

--- onlinebookstore/src/main/java/com/cpadillao/onlinebookstore/repository/BookRepository.java
+++ onlinebookstore/src/main/java/com/cpadillao/onlinebookstore/repository/BookRepository.java
@@ -1,6 +1,11 @@
 package com.cpadillao.onlinebookstore.repository;
 
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.cpadillao.onlinebookstore.entity.Book;
@@ -9,4 +14,6 @@ import com.cpadillao.onlinebookstore.entity.Book;
 @CrossOrigin("http://localhost:4200")
 public interface BookRepository extends JpaRepository<Book, Long>{
 
+	@RestResource(path = "categoryid")
+	Page<Book>findByCategoryId(@Param("id") Long id, Pageable pageable);
 }
